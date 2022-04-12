180cf0cc8b2302b264132f3b9f6e24a715bf6ab4
Leticiavvs13 <leticiavsvieira@gmail.com>
2022-04-12 19:51:42 -0300

    Desenvolvimento dos layouts de listagem e detalhe de partidas.

--- app/src/main/java/com/example/simulatorapp/MainActivity.java
+++ app/src/main/java/com/example/simulatorapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.simulatorapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.simulatorapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+       binding = ActivityMainBinding.inflate(getLayoutInflater());
+       setContentView(binding.getRoot());
     }
 }
---
+++ app/src/test/java/com/example/simulatorapp/ActivityMainBinding.java
@@ -0,0 +1,4 @@
+package com.example.simulatorapp;
+
+public class ActivityMainBinding {
+}
